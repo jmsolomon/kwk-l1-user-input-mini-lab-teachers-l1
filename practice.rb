eb8d46eb6617ab14505bf475abcb0cfb3fe40949
Jason Solomon <jasonmaxsolomon@gmail.com>
2018-07-25 01:54:43 +0000

    automatically backed up by learn

---
+++ practice.rb
@@ -0,0 +1,9 @@
+# def greeting(my_name, my_greeting = "Hi")
+# puts "#{my_greeting}! I'm #{my_name}"
+# end
+ 
+# greeting("Karlie") #> Hi! I'm Karlie
+# greeting("Cardi B", "Heyyyyyy") #> "Heyyyyyy! I'm Cardi B"
+
+
+1 == 1 && 1 <= 2
